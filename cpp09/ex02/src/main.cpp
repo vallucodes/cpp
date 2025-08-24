1f076b0b81e9a4eb822985c9e3494ed018e1c52a
Vallu <vallu9@hotmail.com>
2025-08-24 17:55:59 +0300

    addmax amount of operations is now according to table

--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -27,12 +27,27 @@ void	fill_mainchain_new_keys( std::vector<int>& main_chain, std::vector<int>& ne
 		main_chain.insert(main_chain.begin(), new_pairs[main_chain[0]]);
 }
 
-size_t	binary_insert_pos( std::vector<int>& main_chain, int value) {
-	int left = 0;
-	int right = main_chain.size();
+size_t	related_key( std::vector<int>& main_chain, int value, std::unordered_map<int, int>& new_pairs ) {
+	for (auto& pair : new_pairs)
+	{
+		if (pair.second == value)
+		{
+			for (size_t j = 0; j < main_chain.size(); ++j)
+			{
+				if (main_chain[j] == pair.first)
+					return j;
+			}
+		}
+	}
+	return main_chain.size();
+}
+
+size_t	binary_insert_pos( std::vector<int>& main_chain, int value, std::unordered_map<int, int>& new_pairs ) {
+	size_t left = 0;
+	size_t right = related_key(main_chain, value, new_pairs);
 	while (left < right)
 	{
-		int mid = left + (right - left) / 2;
+		size_t mid = left + (right - left) / 2;
 		if (main_chain[mid] < value)
 			left = mid + 1;
 		else
@@ -76,8 +91,11 @@ std::vector<int>	fill_full_b_chain( std::vector<int>& main_chain, std::unordered
 		size_t new_additions = t_curr - t_prev;
 		for (size_t j = 0; j < new_additions; ++j)
 		{
-			if ((j == jacobsthal_sequence_rounds - 1) && remainder.has_value())
+			if (j == 0 && (i == jacobsthal_sequence_rounds - 1) && remainder.has_value()) //identify leftover
+			{
 				full_b_chain.push_back(remainder.value());
+				continue ;
+			}
 			size_t	b_pos = t_curr - j;
 			if (b_pos > (main_chain.size() - 1))
 				break ;
@@ -94,7 +112,7 @@ void	fill_mainchain( std::vector<int>& main_chain, std::vector<int>& new_keys, s
 	for (size_t i = 0; i < full_b_chain.size(); ++i)
 	{
 		size_t value_to_insert = full_b_chain[i];
-		size_t new_pos = binary_insert_pos(main_chain, value_to_insert);
+		size_t new_pos = binary_insert_pos(main_chain, value_to_insert, new_pairs);
 		main_chain.insert(main_chain.begin() + new_pos, value_to_insert);
 	}
 	print_mainchain(main_chain);
@@ -106,7 +124,7 @@ std::vector<int>	recursive_splitting( std::vector<int>& keys, int level ) {
 	std::vector<int> new_keys;
 	std::optional<int> remainder = std::nullopt;
 
-	if (keys.size() < 3)
+	if (keys.size() < 2)
 		return {};
 	if (keys.size() % 2 != 0)
 	{
@@ -130,29 +148,26 @@ std::vector<int>	recursive_splitting( std::vector<int>& keys, int level ) {
 	print_map(new_keys, new_pairs, level);
 	std::vector<int>	main_chain;
 	main_chain = recursive_splitting(new_keys, level);
-	if (new_keys.size() == 2)
-	{
-		if (new_keys[0] > new_keys[1])
-			swap_keys(new_keys, 1, 0);
-		comparisons++;
-	}
 	fill_mainchain(main_chain, new_keys, new_pairs, remainder);
 	return (main_chain);
 }
 
 int	main()
 {
-	// int size = 5;
-	std::vector<int> arr_to_sort = {3, 0, 2, 17, 4, 15, 20, 1, 5, 9, 7, 18, 8, 16, 19, 6};
+	// int size = 19;
+	// std::vector<int> arr_to_sort = {3, 0, 2, 17, 4, 15, 20, 1, 5, 9, 7, 18, 8, 16, 19, 6};
 	// std::vector<int> arr_to_sort = {3, 0, 2, 20, 4, 15, 17, 8, 9, 1, 12, 11};
+	// std::vector<int> arr_to_sort = {5, 0, 6, 10, 7, 8, 4, 9, 1};
+	std::vector<int> arr_to_sort = {3, 0, 2, 5, 6, 1, 9, 10};
 	// std::vector<int> arr_to_sort = {17, 8, 9, 1, 12, 11, 3, 0};
-	// std::vector<int> arr_to_sort = {17, 8, 9, 1, 12, 11};
+	// std::vector<int> arr_to_sort = {1, 3, 9, 0, 4, 8, 2};
+	// std::vector<int> arr_to_sort = {2, 9, 4, 6, 3, 7};
 	// std::vector<int> arr_to_sort = {17, 8, 9, 1, 12};
 	// std::vector<int> arr_to_sort;
 	// srand(time(0));
 	// for (int i = 0; i < size; ++i)
 	// {
-	// 	arr_to_sort.push_back(rand() % 10000);
+	// 	arr_to_sort.push_back(rand() % 100000);
 	// 	std::cout << arr_to_sort[i] << std::endl;
 	// }
 	recursive_splitting(arr_to_sort, 0);
